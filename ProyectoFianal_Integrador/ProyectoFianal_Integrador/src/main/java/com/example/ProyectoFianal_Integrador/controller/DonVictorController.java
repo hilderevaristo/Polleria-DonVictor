dc6a2fddc661e8a392dce16fc96b85bbb986c3e1
Hilder Evaristo <hilderevaristo94@gmail.com>
2026-06-28 21:20:12 -0500

    nuevo_accion

--- ProyectoFianal_Integrador/ProyectoFianal_Integrador/src/main/java/com/example/ProyectoFianal_Integrador/controller/DonVictorController.java
+++ ProyectoFianal_Integrador/ProyectoFianal_Integrador/src/main/java/com/example/ProyectoFianal_Integrador/controller/DonVictorController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -444,4 +445,6 @@ public Map<String, Object> procesarPedido(@RequestBody Map<String, Object> pedid
     
     return response;
 }
+
+
 }
