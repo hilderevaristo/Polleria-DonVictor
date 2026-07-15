8bf552731547b9e76d75f1553f6f8c14ca4bd4a2
Hilder Evaristo <hilderevaristo94@gmail.com>
2026-07-15 00:34:48 -0500

    nuevo

--- ProyectoFianal_Integrador/ProyectoFianal_Integrador/src/main/java/com/example/ProyectoFianal_Integrador/controller/PerfilController.java
+++ ProyectoFianal_Integrador/ProyectoFianal_Integrador/src/main/java/com/example/ProyectoFianal_Integrador/controller/PerfilController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/perfil")
